d9ba7847612415870f2eec4ec181538e3eb2384f
changer-changer <2125984387@qq.com>
2026-03-22 12:12:25 +0800

    Add DP Advanced branch with 5 open-source diffusion policy models
    - RDP: <1ms, tactile-visual fusion, 309⭐
    - ManiCM: 17ms, consistency distillation, 123⭐
    - Consistency Policy: 1-2ms, CTM distillation, 74⭐
    - DMPO/MP1: 0.6ms, fastest, mean flow, 40⭐
    - ImplicitRDP: end-to-end single network, 22⭐
    
    All verified open source, 8GB GPU feasible

--- dist/assets/chevron-right-BVPt_Olv.js
+++ dist/assets/chevron-right-BVPt_Olv.js
@@ -1,6 +1,6 @@
-import{q as p}from"./index-CNcDoVHp.js";/**
+import{c as t}from"./index-CBmNcFNN.js";/**
  * @license lucide-react v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const o=p("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{o as p};
+ */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
